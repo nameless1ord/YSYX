d91493da0d81afa611ec697329fae219c1a6c57e
nameless1ord <283190940@qq.com>
2023-09-11 16:08:08 +0800

    onoff_nvboard finish

---
+++ ysyx-workbench/预学习/Verilator_pre_exercise/nvboard_work/onoff_switch/build/obj_dir/Vtop___024root__DepSet_heccd7ead__0.cpp
@@ -0,0 +1,178 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vtop.h for the primary calling header
+
+#include "verilated.h"
+
+#include "Vtop___024root.h"
+
+VL_INLINE_OPT void Vtop___024root___ico_sequent__TOP__0(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___ico_sequent__TOP__0\n"); );
+    // Body
+    vlSelf->ledr = (((IData)(vlSelf->top__DOT__led1__DOT__led) 
+                     << 8U) | (1U & ((IData)(vlSelf->sw) 
+                                     ^ ((IData)(vlSelf->sw) 
+                                        >> 1U))));
+}
+
+void Vtop___024root___eval_ico(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_ico\n"); );
+    // Body
+    if (vlSelf->__VicoTriggered.at(0U)) {
+        Vtop___024root___ico_sequent__TOP__0(vlSelf);
+    }
+}
+
+void Vtop___024root___eval_act(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_act\n"); );
+}
+
+VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___nba_sequent__TOP__0\n"); );
+    // Init
+    CData/*7:0*/ __Vdly__top__DOT__led1__DOT__led;
+    __Vdly__top__DOT__led1__DOT__led = 0;
+    IData/*31:0*/ __Vdly__top__DOT__led1__DOT__count;
+    __Vdly__top__DOT__led1__DOT__count = 0;
+    // Body
+    __Vdly__top__DOT__led1__DOT__count = vlSelf->top__DOT__led1__DOT__count;
+    __Vdly__top__DOT__led1__DOT__led = vlSelf->top__DOT__led1__DOT__led;
+    if (vlSelf->rst) {
+        __Vdly__top__DOT__led1__DOT__led = 1U;
+        __Vdly__top__DOT__led1__DOT__count = 0U;
+    } else {
+        if ((0U == vlSelf->top__DOT__led1__DOT__count)) {
+            __Vdly__top__DOT__led1__DOT__led = ((0xfeU 
+                                                 & ((IData)(vlSelf->top__DOT__led1__DOT__led) 
+                                                    << 1U)) 
+                                                | (1U 
+                                                   & ((IData)(vlSelf->top__DOT__led1__DOT__led) 
+                                                      >> 7U)));
+        }
+        __Vdly__top__DOT__led1__DOT__count = ((0x4c4b40U 
+                                               <= vlSelf->top__DOT__led1__DOT__count)
+                                               ? 0U
+                                               : ((IData)(1U) 
+                                                  + vlSelf->top__DOT__led1__DOT__count));
+    }
+    vlSelf->top__DOT__led1__DOT__count = __Vdly__top__DOT__led1__DOT__count;
+    vlSelf->top__DOT__led1__DOT__led = __Vdly__top__DOT__led1__DOT__led;
+    vlSelf->ledr = (((IData)(vlSelf->top__DOT__led1__DOT__led) 
+                     << 8U) | (1U & ((IData)(vlSelf->sw) 
+                                     ^ ((IData)(vlSelf->sw) 
+                                        >> 1U))));
+}
+
+void Vtop___024root___eval_nba(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_nba\n"); );
+    // Body
+    if (vlSelf->__VnbaTriggered.at(0U)) {
+        Vtop___024root___nba_sequent__TOP__0(vlSelf);
+    }
+}
+
+void Vtop___024root___eval_triggers__ico(Vtop___024root* vlSelf);
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___dump_triggers__ico(Vtop___024root* vlSelf);
+#endif  // VL_DEBUG
+void Vtop___024root___eval_triggers__act(Vtop___024root* vlSelf);
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___dump_triggers__act(Vtop___024root* vlSelf);
+#endif  // VL_DEBUG
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vtop___024root___dump_triggers__nba(Vtop___024root* vlSelf);
+#endif  // VL_DEBUG
+
+void Vtop___024root___eval(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval\n"); );
+    // Init
+    CData/*0:0*/ __VicoContinue;
+    VlTriggerVec<1> __VpreTriggered;
+    IData/*31:0*/ __VnbaIterCount;
+    CData/*0:0*/ __VnbaContinue;
+    // Body
+    vlSelf->__VicoIterCount = 0U;
+    __VicoContinue = 1U;
+    while (__VicoContinue) {
+        __VicoContinue = 0U;
+        Vtop___024root___eval_triggers__ico(vlSelf);
+        if (vlSelf->__VicoTriggered.any()) {
+            __VicoContinue = 1U;
+            if (VL_UNLIKELY((0x64U < vlSelf->__VicoIterCount))) {
+#ifdef VL_DEBUG
+                Vtop___024root___dump_triggers__ico(vlSelf);
+#endif
+                VL_FATAL_MT("/home/wzm/YSYX/ysyx-workbench/预学习/Verilator_pre_exercise/nvboard_work/onoff_switch/vsrc/top.v", 1, "", "Input combinational region did not converge.");
+            }
+            vlSelf->__VicoIterCount = ((IData)(1U) 
+                                       + vlSelf->__VicoIterCount);
+            Vtop___024root___eval_ico(vlSelf);
+        }
+    }
+    __VnbaIterCount = 0U;
+    __VnbaContinue = 1U;
+    while (__VnbaContinue) {
+        __VnbaContinue = 0U;
+        vlSelf->__VnbaTriggered.clear();
+        vlSelf->__VactIterCount = 0U;
+        vlSelf->__VactContinue = 1U;
+        while (vlSelf->__VactContinue) {
+            vlSelf->__VactContinue = 0U;
+            Vtop___024root___eval_triggers__act(vlSelf);
+            if (vlSelf->__VactTriggered.any()) {
+                vlSelf->__VactContinue = 1U;
+                if (VL_UNLIKELY((0x64U < vlSelf->__VactIterCount))) {
+#ifdef VL_DEBUG
+                    Vtop___024root___dump_triggers__act(vlSelf);
+#endif
+                    VL_FATAL_MT("/home/wzm/YSYX/ysyx-workbench/预学习/Verilator_pre_exercise/nvboard_work/onoff_switch/vsrc/top.v", 1, "", "Active region did not converge.");
+                }
+                vlSelf->__VactIterCount = ((IData)(1U) 
+                                           + vlSelf->__VactIterCount);
+                __VpreTriggered.andNot(vlSelf->__VactTriggered, vlSelf->__VnbaTriggered);
+                vlSelf->__VnbaTriggered.set(vlSelf->__VactTriggered);
+                Vtop___024root___eval_act(vlSelf);
+            }
+        }
+        if (vlSelf->__VnbaTriggered.any()) {
+            __VnbaContinue = 1U;
+            if (VL_UNLIKELY((0x64U < __VnbaIterCount))) {
+#ifdef VL_DEBUG
+                Vtop___024root___dump_triggers__nba(vlSelf);
+#endif
+                VL_FATAL_MT("/home/wzm/YSYX/ysyx-workbench/预学习/Verilator_pre_exercise/nvboard_work/onoff_switch/vsrc/top.v", 1, "", "NBA region did not converge.");
+            }
+            __VnbaIterCount = ((IData)(1U) + __VnbaIterCount);
+            Vtop___024root___eval_nba(vlSelf);
+        }
+    }
+}
+
+#ifdef VL_DEBUG
+void Vtop___024root___eval_debug_assertions(Vtop___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root___eval_debug_assertions\n"); );
+    // Body
+    if (VL_UNLIKELY((vlSelf->clk & 0xfeU))) {
+        Verilated::overWidthError("clk");}
+    if (VL_UNLIKELY((vlSelf->rst & 0xfeU))) {
+        Verilated::overWidthError("rst");}
+    if (VL_UNLIKELY((vlSelf->ps2_clk & 0xfeU))) {
+        Verilated::overWidthError("ps2_clk");}
+    if (VL_UNLIKELY((vlSelf->ps2_data & 0xfeU))) {
+        Verilated::overWidthError("ps2_data");}
+}
+#endif  // VL_DEBUG
