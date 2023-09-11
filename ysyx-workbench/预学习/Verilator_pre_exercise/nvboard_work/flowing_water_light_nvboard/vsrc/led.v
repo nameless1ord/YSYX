c3fafe82f34d14ce150cf0fb503c12a2015b91bc
nameless1ord <283190940@qq.com>
2023-09-11 16:56:10 +0800

    flowing water light finish

---
+++ ysyx-workbench/预学习/Verilator_pre_exercise/nvboard_work/flowing_water_light_nvboard/vsrc/led.v
@@ -0,0 +1,15 @@
+module led(
+  input clk,
+  input rst,
+  input [7:0] sw,
+  output reg [15:0] ledr
+);
+  reg [31:0] count;
+  always @(posedge clk) begin
+    if (rst) begin ledr <= 1; count <= 0; end
+    else begin
+      if (count == 0) ledr <= {ledr[14:0], ledr[15]};
+      count <= (count >= 5000000 ? 32'b0 : count + 1);
+    end
+  end
+endmodule
